33cc073610eaeb65768ee715aa63ded5d6fc79c5
Benjamin Kramskoi <ben.kramskoi@gmail.com>
2020-11-29 10:59:52 -0500

    last commit

--- app.py
+++ app.py
@@ -1,32 +1,36 @@
-from flask import Flask, render_template, redirect,jsonify
+# Import Dependencies
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
+# Create Instance of Flask App
 app = Flask(__name__)
 
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars")
-
-@app.route("/")
-def home():
-
-    mars = mongo.db.collection.find_one()
-
-    return render_template("index.html", mars=mars)
+# Set up mongo connection in line 
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 
-# Route that will trigger the scrape function
-@app.route("/scrape")
+@app.route('/')
+def index():
+    # Finding one document from our mongoDB and return it
+    mars_data = mongo.db.mars_data.find_one()
+
+    # Pass that listing to render template
+    return render_template('index.html', mars_data = mars_data)
+
+
+@app.route('/scrape')
 def scrape():
-
-    # Run the scrape function
-    mars_data = scrape_mars.scrape_info()
-
-    # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_data, upsert=True)
-
-    # Redirect back to home page
-    return redirect("/",code=302)
+    # Create mars info collection
+    mars = mongo.db.mars_data
+    mars_data = scrape_mars.scrape()
+    mars.update(
+        {},
+        mars_data,
+        upsert=True
+    )
+    return redirect("/", code=302)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
